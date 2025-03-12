138bc0bf539bfd2555231b309f5801299c9ed35f
Trey McGarity <trey.mcgarity99@gmail.com>
2025-03-12 00:32:46 -0500

    Payment update endpoint created

--- src/routes/bookingRouter.js
+++ src/routes/bookingRouter.js
@@ -1,22 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const { getAllBookings, getBookingById, updateBooking, deleteBooking, reserveFlight, getCustomerBookings } = require('../../controllers/bookingController');
+const { getAllBookings, getBookingById, updateBooking, deleteBooking, reserveFlight, getCustomerBookings, updatePaymentStatus } = require('../../controllers/bookingController');
 const authMiddleware = require('../../controllers/middleware/AuthMiddleware'); // Middleware to authenticate user
 
 //POST Booking
-router.post('/bookFlight', authMiddleware, reserveFlight); // Route for fetching logged-in Booking details
+router.post('/bookFlight', authMiddleware, reserveFlight); // Route for logged-in Booking addition
 
 // Get CustomerBookings
 router.get('/customerReservations', authMiddleware, getCustomerBookings);
+
+//PUT updatePaymentStatus
+router.put('/:id/updateStatus', authMiddleware, updatePaymentStatus);
 
 // GET endpoint
 router.get('/', authMiddleware, getAllBookings);
 
 // GET Booking by ID
-router.get('/:id', getBookingById);
+router.get('/:id', authMiddleware, getBookingById);
 
 // PUT (update) a Booking by ID
-router.put('/:id', updateBooking);
+router.put('/:id', authMiddleware, updateBooking);
 
 // DELETE a Booking by ID
 router.delete('/:id', deleteBooking);
